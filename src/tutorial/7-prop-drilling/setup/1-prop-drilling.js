2c1c3eae03cf5e889df3baba42ecc3e7d7feb9c4
Prateek <prateek.badjatya@gmail.com>
2021-07-26 06:12:44 +0530

    complete props drilling

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,44 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import { data } from "../../../data";
 // more components
 // fix - context api, redux (for more complex cases)
 
+// passing unnnessecary props to compoenent that was not useful for that compoent
+// here removePersonis not useful for list
+// here it is useful fot single person component
+const List = ({ pepole, removePerson }) => {
+  return (
+    <>
+      {pepole.map(person => (
+        <SinglePerson removePerson={removePerson} key={person.id} {...person} />
+      ))}
+    </>
+  );
+};
+
+const SinglePerson = ({ id, name, removePerson }) => {
+  // let { id, name } = person;
+  console.log("nbame", id, name);
+  return (
+    <div key={id} className="item">
+      <h4>{name}</h4>
+      <button onClick={() => removePerson(id)}>Remove</button>
+    </div>
+  );
+};
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [pepole, setPepole] = useState(data);
+  const removePerson = id => {
+    setPepole(p => {
+      return p.filter(person => person.id !== id);
+    });
+  };
+  return (
+    <section>
+      <h3>Props Drilling</h3>
+      <List pepole={pepole} removePerson={removePerson} />
+    </section>
+  );
 };
 
 export default PropDrilling;
