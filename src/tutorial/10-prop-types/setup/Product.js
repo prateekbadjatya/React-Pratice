1da49ad086b1f5d5ececaeb377e4c6f77eae630c
prateekbadjatya <prateek.badjatya@gmail.com>
2021-07-28 01:57:22 +0530

    add prop types for default value

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,31 @@
-import React from 'react';
-
-const Product = () => {
-  return <article className='product'>single product</article>;
+import React from "react";
+import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
+const Product = ({ image, name, price }) => {
+  // console.log("image", image);
+  // let url = image && image.url ? image.url : null;
+  // console.log(url);
+  return (
+    <article className="product">
+      {
+        <div>
+          <h4>{name}</h4>
+          <p>${price}</p>
+          <img alt={name} src={image.url} />
+        </div>
+      }
+    </article>
+  );
 };
 
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired
+};
+Product.defaultProps = {
+  image: "default name",
+  price: 3.99,
+  image: { url: defaultImage }
+};
 export default Product;
