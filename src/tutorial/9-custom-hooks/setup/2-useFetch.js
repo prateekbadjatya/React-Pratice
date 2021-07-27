fee1f007bc6a4debbaaad1b0443242ea18b820ac
prateekbadjatya <prateek.badjatya@gmail.com>
2021-07-28 01:27:48 +0530

    add usefetch feature

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export const useFetch = () => {};
+// function name always start with use otherwise here it will give an error
+export const useFetch = url => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      const response = await fetch(url);
+      const products = await response.json();
+      setProducts(products);
+      setLoading(false);
+    } catch (error) {
+      setProducts([]);
+      setLoading(true);
+    }
+  };
+  useEffect(() => {
+    getProducts();
+  }, [url]);
+  return {
+    loading,
+    products
+  };
+};
